1a39c11e28fadabd1f58a1d412b6800fcf27ea27
Chris <csjones@users.noreply.github.com>
2020-09-04 16:21:50 -0700

    Updated Swift tools version and supported versions (#22)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -66,6 +66,6 @@ let package = Package(
             ]
         )
     ],
-    swiftLanguageVersions: [.v5],
+    swiftLanguageVersions: [.v4, .v5],
     cLanguageStandard: .c89
 )
