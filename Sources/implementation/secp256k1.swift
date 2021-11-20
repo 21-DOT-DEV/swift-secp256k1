4b2778c02c4aa5d18ae4bb6429449fc78a13fce2
Chris <csjones@users.noreply.github.com>
2021-11-19 17:36:55 -0700

    Cleaning up key pair creation

--- Sources/implementation/secp256k1.swift
+++ Sources/implementation/secp256k1.swift
@@ -24,8 +24,8 @@ extension secp256k1 {
             private var baseKey: secp256k1.Signing.PrivateKeyImplementation
 
             /// Creates a random secp256k1 private key for signing
-            public init() {
-                self.baseKey = secp256k1.Signing.PrivateKeyImplementation()
+            public init() throws {
+                self.baseKey = try secp256k1.Signing.PrivateKeyImplementation()
             }
 
             /// The associated public key for verifying signatures done with this private key.
@@ -58,7 +58,7 @@ extension secp256k1 {
             /// Generated secp256k1 public key.
             private var baseKey: secp256k1.Signing.PublicKeyImplementation
 
-            /// Generates a secp256k1 public key from from a raw representation.
+            /// Generates a secp256k1 public key from a raw representation.
             /// - Parameter data: A raw representation of the key.
             /// - Throws: An error is thrown when the raw representation does not create a public key.
             public init<D: ContiguousBytes>(rawRepresentation data: D) throws {
@@ -90,37 +90,9 @@ extension secp256k1.Signing {
     @usableFromInline struct PrivateKeyImplementation {
         /// Backing private key object
         var _privateKey: SecureBytes
+
         /// Backing public key object
         @usableFromInline var _publicKey: [UInt8]
-
-        /// Backing initialization that creates a random secp256k1 private key for signing
-        @usableFromInline init() {
-            // Initialize context
-            let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
-
-            // Destroy context after creation
-            defer { secp256k1_context_destroy(context) }
-
-            // Setup private and public key variables
-            var pubkeyLen = 33
-            var cPubkey = secp256k1_pubkey()
-            var pubkey = [UInt8](repeating: 0, count: 33)
-            let privatekey = SecureBytes(count: 128)
-            let privkey = Data(privatekey).withUnsafeBytes({ keyBytesPtr in Array(keyBytesPtr) })
-
-            // Verify the context and keys are setup correctly
-            guard secp256k1_context_randomize(context, privkey) == 1,
-                secp256k1_ec_pubkey_create(context, &cPubkey, privkey) == 1,
-                secp256k1_ec_pubkey_serialize(context, &pubkey, &pubkeyLen, &cPubkey, UInt32(SECP256K1_EC_COMPRESSED)) == 1 else {
-                self._privateKey = privatekey
-                self._publicKey = pubkey
-                return
-            }
-
-            // Save
-            self._privateKey = privatekey
-            self._publicKey = pubkey
-        }
 
         /// Backing implementation for a public key object
         @usableFromInline var publicKey: secp256k1.Signing.PublicKeyImplementation {
@@ -132,40 +104,34 @@ extension secp256k1.Signing {
             return self._privateKey
         }
 
+        /// A data representation of the backing private key
+        @usableFromInline var rawRepresentation: Data {
+            return Data(self._privateKey)
+        }
+
+        /// Private key length
+        static var byteCount: Int = 2 * secp256k1.CurveDetails.coordinateByteCount
+
+        /// Backing initialization that creates a random secp256k1 private key for signing
+        @usableFromInline init() throws {
+            let privateKey = SecureBytes(count: Self.byteCount)
+            let pubKey = try secp256k1.Signing.PublicKeyImplementation.generatePublicKey(bytes: Data(privateKey).bytes)
+
+            // Save
+            self._privateKey = privateKey
+            self._publicKey = pubKey
+        }
+
         /// Backing initialization that creates a secp256k1 private key for signing from a data representation.
         /// - Parameter data: A raw representation of the key.
         /// - Throws: An error is thrown when the raw representation does not create a private key for signing.
         init<D: ContiguousBytes>(rawRepresentation data: D) throws {
-            // Initialize context
-            let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
-
-            // Destroy context after creation
-            defer { secp256k1_context_destroy(context) }
-
-            // Setup private and public key variables
-            var pubkeyLen = 33
-            var cPubkey = secp256k1_pubkey()
-            var pubkey = [UInt8](repeating: 0, count: 33)
-            let privatekey = SecureBytes(bytes: data)
-            let privkey = data.withUnsafeBytes({ keyBytesPtr in Array(keyBytesPtr) })
-
-            // Verify the context and keys are setup correctly
-            guard secp256k1_context_randomize(context, privkey) == 1,
-                  secp256k1_ec_pubkey_create(context, &cPubkey, privkey) == 1,
-                  secp256k1_ec_pubkey_serialize(context, &pubkey, &pubkeyLen, &cPubkey, UInt32(SECP256K1_EC_COMPRESSED)) == 1 else {
-                self._privateKey = privatekey
-                self._publicKey = pubkey
-                return
-            }
+            let privateKey = SecureBytes(bytes: data)
+            let pubKey = try secp256k1.Signing.PublicKeyImplementation.generatePublicKey(bytes: Data(privateKey).bytes)
 
             // Save
-            self._privateKey = privatekey
-            self._publicKey = pubkey
-        }
-
-        /// A data representation of the backing private key
-        @usableFromInline var rawRepresentation: Data {
-            return Data(self._privateKey)
+            self._privateKey = privateKey
+            self._publicKey = pubKey
         }
     }
 
@@ -174,22 +140,53 @@ extension secp256k1.Signing {
         /// Implementation public key object
         @usableFromInline var keyBytes: [UInt8]
 
-        /// Backing initialization that generates a secp256k1 public key from from a raw representation.
+        /// A data representation of the backing public key
+        var rawRepresentation: Data {
+            return Data(self.keyBytes)
+        }
+
+        /// Backing initialization that generates a secp256k1 public key from a raw representation.
         /// - Parameter data: A raw representation of the key.
         /// - Throws: An error is thrown when the raw representation does not create a public key.
         @inlinable init<D: ContiguousBytes>(rawRepresentation data: D) throws {
             self.keyBytes = data.withUnsafeBytes({ keyBytesPtr in Array(keyBytesPtr) })
         }
 
-        /// Backing initialization that sets the public key from from a public key object.
+        /// Backing initialization that sets the public key from a public key object.
         /// - Parameter keyBytes: a public key object
         init(_ keyBytes: [UInt8]) {
             self.keyBytes = keyBytes
         }
 
-        /// A data representation of the backing public key
-        var rawRepresentation: Data {
-            return Data(self.keyBytes)
+        /// Generates a secp256k1 public key from bytes representation.
+        /// - Parameter privateKey: a private key object
+        /// - Returns: a public key object
+        /// - Throws: An error is thrown when the bytes does not create a public key. 
+        static func generatePublicKey(bytes privateKey: [UInt8]) throws -> [UInt8] {
+            guard privateKey.count == secp256k1.Signing.PrivateKeyImplementation.byteCount else {
+                throw secp256k1Error.incorrectKeySize
+            }
+
+            // Initialize context
+            let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
+
+            // Destroy context after creation
+            defer { secp256k1_context_destroy(context) }
+
+            // Setup private and public key variables
+            var pubKeyLen = 33
+            var cPubKey = secp256k1_pubkey()
+            var pubKey = [UInt8](repeating: 0, count: 33)
+            let privKey = privateKey
+
+            // Verify the context and keys are setup correctly
+            guard secp256k1_context_randomize(context, privKey) == 1,
+                secp256k1_ec_pubkey_create(context, &cPubKey, privKey) == 1,
+                secp256k1_ec_pubkey_serialize(context, &pubKey, &pubKeyLen, &cPubKey, UInt32(SECP256K1_EC_COMPRESSED)) == 1 else {
+                    throw secp256k1Error.underlyingCryptoError
+            }
+
+            return pubKey
         }
     }
 }
