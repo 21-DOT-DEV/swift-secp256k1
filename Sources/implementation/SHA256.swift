d92bc54544df3bcec6d06e8646cfbda5f30ad9c1
David Scheutz <dscheutz@lfgpay.xyz>
2022-07-14 10:40:34 -0700

    introduce SHA32BytesDigest

--- Sources/implementation/SHA256.swift
+++ Sources/implementation/SHA256.swift
@@ -15,19 +15,19 @@ public enum SHA256 {
     /// Computes a digest of the data.
     /// - Parameter data: The data to be hashed
     /// - Returns: The computed digest
-    public static func hash<D: DataProtocol>(data: D) -> SHA256Digest {
+    public static func hash<D: DataProtocol>(data: D) -> SHA32BytesDigest {
         let stringData = Array(data)
         var output = [UInt8](repeating: 0, count: 32)
 
         secp256k1_swift_sha256(&output, stringData, stringData.count)
 
-        return convert(output)
+        return .init(output)
     }
 
     /// Computes a digest of the data.
     /// - Parameter data: The data to be hashed
     /// - Returns: The computed digest
-    public static func taggedHash<D: DataProtocol>(tag: [UInt8], data: D) throws -> SHA256Digest {
+    public static func taggedHash<D: DataProtocol>(tag: [UInt8], data: D) throws -> SHA32BytesDigest {
         let messageBytes = Array(data)
         var output = [UInt8](repeating: 0, count: 32)
 
@@ -35,15 +35,6 @@ public enum SHA256 {
             throw secp256k1Error.underlyingCryptoError
         }
 
-        return convert(output)
-    }
-
-    private static func convert(_ output: [UInt8]) -> SHA256Digest {
-        let first = output[0..<8].withUnsafeBytes { $0.load(as: UInt64.self) }
-        let second = output[8..<16].withUnsafeBytes { $0.load(as: UInt64.self) }
-        let third = output[16..<24].withUnsafeBytes { $0.load(as: UInt64.self) }
-        let forth = output[24..<32].withUnsafeBytes { $0.load(as: UInt64.self) }
-
-        return SHA256Digest(bytes: (first, second, third, forth))
+        return .init(output)
     }
 }
