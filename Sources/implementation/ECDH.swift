2f229cd40435af7ad18df396b26cbeaecd9fcdef
Chris <csjones@users.noreply.github.com>
2022-07-22 23:26:59 -0700

    Public Key and ECDH revisions

--- Sources/implementation/ECDH.swift
+++ Sources/implementation/ECDH.swift
@@ -17,20 +17,27 @@ public extension secp256k1 {
     enum KeyAgreement {
         public struct PublicKey /*: NISTECPublicKey */ {
             let baseKey: PublicKeyImplementation
-
+            
             /// Creates a secp256k1 public key for key agreement from a collection of bytes.
             /// - Parameters:
             ///   - data: A raw representation of the public key as a collection of contiguous bytes.
             ///   - xonly: A raw representation of the xonly key as a collection of contiguous bytes.
             ///   - format: the format of the public key object
-            public init<D: ContiguousBytes>(rawRepresentation data: D, xonly: D, keyParity: Int32, format: secp256k1.Format) {
-                self.baseKey = PublicKeyImplementation(rawRepresentation: data, xonly: xonly, keyParity: keyParity, format: format)
+            public init<D: ContiguousBytes>(rawRepresentation data: D, format: secp256k1.Format = .compressed) throws {
+                self.baseKey = try PublicKeyImplementation(rawRepresentation: data, format: format)
             }
 
             /// Initializes a secp256k1 public key for key agreement.
             /// - Parameter baseKey: generated secp256k1 public key.
             init(baseKey: PublicKeyImplementation) {
                 self.baseKey = baseKey
+            }
+            
+            /// The associated x-only public key for verifying Schnorr signatures.
+            ///
+            /// - Returns: The associated x-only public key
+            public var xonly: secp256k1.KeyAgreement.XonlyKey {
+                XonlyKey(baseKey: baseKey.xonly)
             }
 
             /// A data representation of the public key
@@ -39,8 +46,26 @@ public extension secp256k1 {
             /// Implementation public key object
             var bytes: [UInt8] { baseKey.bytes }
         }
+        
+        public struct XonlyKey {
+            /// Generated secp256k1 x-only public key.
+            private let baseKey: XonlyKeyImplementation
+            
+            /// A data representation of the backing x-only public key
+            public var rawRepresentation: Data { baseKey.rawRepresentation }
+            
+            /// A boolean that will be set to true if the point encoded by xonly is the
+            /// negation of the pubkey and set to false otherwise.
+            public var parity: Bool { baseKey.keyParity.boolValue }
+            
+            /// Initializes a secp256k1 x-only key for key agreement.
+            /// - Parameter baseKey: generated secp256k1 public key.
+            init(baseKey: XonlyKeyImplementation) {
+                self.baseKey = baseKey
+            }
+        }
 
-        public struct PrivateKey /*: NISTECPrivateKey */ {
+        public struct PrivateKey {
             let baseKey: PrivateKeyImplementation
 
             /// Creates a random secp256k1 private key for key agreement.
@@ -78,17 +103,40 @@ public extension secp256k1 {
 // MARK: - secp256k1 + DH
 
 extension secp256k1.KeyAgreement.PrivateKey: DiffieHellmanKeyAgreement {
+    /// A pointer to a function that hashes an EC point to obtain an ECDH secret
+    public typealias HashFunctionType = @convention(c) (
+        UnsafeMutablePointer<UInt8>?,
+        UnsafePointer<UInt8>?,
+        UnsafePointer<UInt8>?,
+        UnsafeMutableRawPointer?
+    ) -> Int32
+    
     /// Performs a key agreement with provided public key share.
     ///
     /// - Parameter publicKeyShare: The public key to perform the ECDH with.
     /// - Returns: Returns a shared secret
     /// - Throws: An error occurred while computing the shared secret
-    public func sharedSecretFromKeyAgreement(with publicKeyShare: secp256k1.KeyAgreement.PublicKey) throws -> SharedSecret {
+    func sharedSecretFromKeyAgreement(with publicKeyShare: secp256k1.KeyAgreement.PublicKey) throws -> SharedSecret {
+        try sharedSecretFromKeyAgreement(with: publicKeyShare, handler: nil)
+    }
+
+    /// Performs a key agreement with provided public key share.
+    /// - Parameters:
+    ///   - publicKeyShare: The public key to perform the ECDH with.
+    ///   - handler: Closure for customizing a hash function; Defaults to nil.
+    /// - Returns: Returns a shared secret
+    /// - Throws: An error occurred while computing the shared secret
+    public func sharedSecretFromKeyAgreement(
+        with publicKeyShare: secp256k1.KeyAgreement.PublicKey,
+        handler: HashFunctionType? = nil,
+        data: UnsafeMutableRawPointer? = nil
+    ) throws -> SharedSecret {
+        let context = secp256k1.Context.raw
         var publicKey = secp256k1_pubkey()
         var sharedSecret = [UInt8](repeating: 0, count: 32)
 
         guard secp256k1_ec_pubkey_parse(secp256k1.Context.raw, &publicKey, publicKeyShare.bytes, publicKeyShare.bytes.count).boolValue,
-              secp256k1_ecdh(secp256k1.Context.raw, &sharedSecret, &publicKey, baseKey.key.bytes, nil, nil).boolValue else {
+              secp256k1_ecdh(context, &sharedSecret, &publicKey, baseKey.key.bytes, handler, data).boolValue else {
             throw secp256k1Error.underlyingCryptoError
         }
 
