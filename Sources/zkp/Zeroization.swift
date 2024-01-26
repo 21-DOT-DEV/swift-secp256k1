0ab1cbd5826d05646296079b6d03aa98e85074b3
Chris <csjones@users.noreply.github.com>
2024-01-26 13:19:03 -0800

    Add visionOS support

--- Sources/zkp/Zeroization.swift
+++ Sources/zkp/Zeroization.swift
@@ -28,7 +28,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
-#if !(os(macOS) || os(iOS) || os(watchOS) || os(tvOS))
+#if !(os(macOS) || os(iOS) || os(watchOS) || os(tvOS) || os(visionOS))
     public typealias errno_t = CInt
 
     @discardableResult
