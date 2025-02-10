75a475aa68a8171cb419830ea3a089a14861cc08
csjones <csjones@users.noreply.github.com>
2025-02-10 11:02:31 -0800

    Updating tests

--- Tests/ZKPTests/P256KTests.swift
+++ Tests/ZKPTests/P256KTests.swift
@@ -7,7 +7,7 @@
 import XCTest
 import Testing
 
-struct NewTestSuite {
+struct P256KTestSuite {
 
     @Test("Compressed Key pair test with raw data")
     func compressedKeypairImplementationWithRaw() {
@@ -21,246 +21,7 @@ struct NewTestSuite {
     }
 }
 
-// Other test definitions
-
 final class P256KTests: XCTestCase, @unchecked Sendable {
-//    /// Uncompressed Key pair test
-//    func testUncompressedKeypairCreation() {
-//        // Initialize context
-//        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
-//
-//        // Destroy context after execution
-//        defer { secp256k1_context_destroy(context) }
-//
-//        // Setup private and public key variables
-//        var pubkeyLen = 65
-//        var cPubkey = secp256k1_pubkey()
-//        var publicKey = [UInt8](repeating: 0, count: pubkeyLen)
-//
-//        let privateKey = try! "14E4A74438858920D8A35FB2D88677580B6A2EE9BE4E711AE34EC6B396D87B5C".bytes
-//
-//        // Verify the context and keys are setup correctly
-//        XCTAssertEqual(secp256k1_context_randomize(context, privateKey), 1)
-//        XCTAssertEqual(secp256k1_ec_pubkey_create(context, &cPubkey, privateKey), 1)
-//        XCTAssertEqual(secp256k1_ec_pubkey_serialize(context, &publicKey, &pubkeyLen, &cPubkey, UInt32(SECP256K1_EC_UNCOMPRESSED)), 1)
-//
-//        let hexString = """
-//        04734B3511150A60FC8CAC329CD5FF804555728740F2F2E98BC4242135EF5D5E4E6C4918116B0866F50C46614F3015D8667FBFB058471D662A642B8EA2C9C78E8A
-//        """
-//
-//        // Define the expected public key
-//        let expectedPublicKey = try! hexString.bytes
-//
-//        // Verify the generated public key matches the expected public key
-//        XCTAssertEqual(expectedPublicKey, publicKey)
-//        XCTAssertEqual(hexString.lowercased(), String(bytes: publicKey))
-//    }
-
-//    /// Compressed Key pair test
-//    func testCompressedKeypairCreation() {
-//        // Initialize context
-//        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
-//
-//        // Destroy context after execution
-//        defer { secp256k1_context_destroy(context) }
-//
-//        // Setup private and public key variables
-//        var pubkeyLen = 33
-//        var cPubkey = secp256k1_pubkey()
-//        var publicKey = [UInt8](repeating: 0, count: pubkeyLen)
-//        let privateKey = try! "B035FCFC6ABF660856C5F3A6F9AC51FCA897BB4E76AD9ACA3EFD40DA6B9C864B".bytes
-//
-//        // Verify the context and keys are setup correctly
-//        XCTAssertEqual(secp256k1_context_randomize(context, privateKey), 1)
-//        XCTAssertEqual(secp256k1_ec_pubkey_create(context, &cPubkey, privateKey), 1)
-//        XCTAssertEqual(secp256k1_ec_pubkey_serialize(context, &publicKey, &pubkeyLen, &cPubkey, UInt32(SECP256K1_EC_COMPRESSED)), 1)
-//
-//        // Define the expected public key
-//        let expectedPublicKey = try! "02EA724B70B48B61FB87E4310871A48C65BF38BF3FDFEFE73C2B90F8F32F9C1794".bytes
-//
-//        // Verify the generated public key matches the expected public key
-//        XCTAssertEqual(expectedPublicKey, publicKey)
-//    }
-
-//    func testECDHBindings() {
-//        // Initialize context
-//        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
-//
-//        // Destroy context after execution
-//        defer { secp256k1_context_destroy(context) }
-//
-//        var point = secp256k1_pubkey()
-//        var res = [UInt8](repeating: 0, count: 32)
-//        var s_one = [UInt8](repeating: 0, count: 32)
-//
-//        s_one[31] = 1
-//
-//        XCTAssertEqual(secp256k1_ec_pubkey_create(context, &point, s_one), 1)
-//        XCTAssertEqual(secp256k1_ecdh(context, &res, &point, s_one, nil, nil), 1)
-//    }
-
-//    func testExtraKeysBindings() {
-//        // Initialize context
-//        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
-//
-//        // Destroy context after execution
-//        defer { secp256k1_context_destroy(context) }
-//
-//        var pubKey = secp256k1_pubkey()
-//        var xOnlyPubKey = secp256k1_xonly_pubkey()
-//        var pk_parity = Int32()
-//
-//        let privateKey = try! "14E4A74438858920D8A35FB2D88677580B6A2EE9BE4E711AE34EC6B396D87B5C".bytes
-//
-//        XCTAssertEqual(secp256k1_ec_pubkey_create(context, &pubKey, privateKey), 1)
-//        XCTAssertEqual(secp256k1_xonly_pubkey_from_pubkey(context, &xOnlyPubKey, &pk_parity, &pubKey), 1)
-//    }
-
-//    func testRecoveryBindings() {
-//        // Initialize context
-//        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
-//
-//        // Destroy context after execution
-//        defer { secp256k1_context_destroy(context) }
-//
-//        var pubKey = secp256k1_pubkey()
-//        var recsig = secp256k1_ecdsa_recoverable_signature()
-//        var message = [UInt8](repeating: 0, count: 32)
-//
-//        let privateKey = try! "14E4A74438858920D8A35FB2D88677580B6A2EE9BE4E711AE34EC6B396D87B5C".bytes
-//
-//        XCTAssertEqual(secp256k1_ec_seckey_verify(context, privateKey), 1)
-//        XCTAssertEqual(secp256k1_ec_pubkey_create(context, &pubKey, privateKey), 1)
-//        XCTAssertEqual(secp256k1_ecdsa_sign_recoverable(context, &recsig, &message, privateKey, nil, nil), 1)
-//    }
-
-//    func testSchnorrBindings() {
-//        // Initialize context
-//        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
-//
-//        // Destroy context after execution
-//        defer { secp256k1_context_destroy(context) }
-//
-//        var keypair = secp256k1_keypair()
-//        var xpubKey = secp256k1_xonly_pubkey()
-//        var xpubKeyBytes = [UInt8](repeating: 0, count: 32)
-//
-//        let privateKey = try! "14E4A74438858920D8A35FB2D88677580B6A2EE9BE4E711AE34EC6B396D87B5C".bytes
-//
-//        XCTAssertEqual(secp256k1_keypair_create(context, &keypair, privateKey), 1)
-//        XCTAssertEqual(secp256k1_keypair_xonly_pub(context, &xpubKey, nil, &keypair), 1)
-//        XCTAssertEqual(secp256k1_xonly_pubkey_serialize(context, &xpubKeyBytes, &xpubKey), 1)
-//
-//        let expectedXPubKey = "734b3511150a60fc8cac329cd5ff804555728740f2f2e98bc4242135ef5d5e4e"
-//
-//        XCTAssertEqual(String(bytes: xpubKeyBytes), expectedXPubKey)
-//    }
-
-    /// Compressed Key pair test
-    func testCompressedKeypairImplementationWithRaw() {
-        let expectedPrivateKey = "7da12cc39bb4189ac72d34fc2225df5cf36aaacdcac7e5a43963299bc8d888ed"
-        let expectedPublicKey = "023521df7b94248ffdf0d37f738a4792cc3932b6b1b89ef71cddde8251383b26e7"
-        let privateKeyBytes = try! expectedPrivateKey.bytes
-        let privateKey = try! secp256k1.Signing.PrivateKey(dataRepresentation: privateKeyBytes)
-
-        // Verify the keys matches the expected keys output
-        XCTAssertEqual(expectedPrivateKey, String(bytes: privateKey.dataRepresentation))
-        XCTAssertEqual(expectedPublicKey, String(bytes: privateKey.publicKey.dataRepresentation))
-    }
-
-    /// SHA256 test
-    func testSha256() {
-        let expectedHashDigest = "f08a78cbbaee082b052ae0708f32fa1e50c5c421aa772ba5dbb406a2ea6be342"
-        let data = "For this sample, this 63-byte string will be used as input data".data(using: .utf8)!
-
-        let digest = SHA256.hash(data: data)
-
-        // Verify the hash digest matches the expected output
-        XCTAssertEqual(expectedHashDigest, String(bytes: Array(digest)))
-    }
-
-    func testShaHashDigest() {
-        let expectedHash = try! "f08a78cbbaee082b052ae0708f32fa1e50c5c421aa772ba5dbb406a2ea6be342".bytes
-        let data = "For this sample, this 63-byte string will be used as input data".data(using: .utf8)!
-
-        let digest = SHA256.hash(data: data)
-
-        let constructedDigest = HashDigest(expectedHash)
-
-        // Verify the generated hash digest matches the manual constructed hash digest
-        XCTAssertEqual(String(bytes: Array(digest)), String(bytes: Array(constructedDigest)))
-    }
-
-    func testSigning() {
-        let expectedDerSignature = "MEQCIHS177uYACnX8HzD+hGbG5X/F4iHuRm2DvTylOCV4fmsAiBWbj0MDud/oVzRqL87JjZpCN+kLl8Egcc/GiOigWJg+A=="
-        let expectedSignature = "rPnhleCU8vQOthm5h4gX/5UbmxH6w3zw1ykAmLvvtXT4YGKBoiMaP8eBBF8upN8IaTYmO7+o0Vyhf+cODD1uVg=="
-        let expectedPrivateKey = "5f6d5afecc677d66fb3d41eee7a8ad8195659ceff588edaf416a9a17daf38fdd"
-        let privateKeyBytes = try! expectedPrivateKey.bytes
-        let privateKey = try! secp256k1.Signing.PrivateKey(dataRepresentation: privateKeyBytes)
-        let messageData = "We're all Satoshi Nakamoto and a bit of Harold Thomas Finney II.".data(using: .utf8)!
-
-        let signature = try! privateKey.signature(for: messageData)
-
-        // Verify the signature matches the expected output
-        XCTAssertEqual(expectedSignature, signature.dataRepresentation.base64EncodedString())
-        XCTAssertEqual(expectedDerSignature, try! signature.derRepresentation.base64EncodedString())
-    }
-
-    func testRecoverySigning() {
-        let expectedDerSignature = "MEQCIHS177uYACnX8HzD+hGbG5X/F4iHuRm2DvTylOCV4fmsAiBWbj0MDud/oVzRqL87JjZpCN+kLl8Egcc/GiOigWJg+A=="
-        let expectedRecoverySignature = "rPnhleCU8vQOthm5h4gX/5UbmxH6w3zw1ykAmLvvtXT4YGKBoiMaP8eBBF8upN8IaTYmO7+o0Vyhf+cODD1uVgE="
-        let expectedSignature = "rPnhleCU8vQOthm5h4gX/5UbmxH6w3zw1ykAmLvvtXT4YGKBoiMaP8eBBF8upN8IaTYmO7+o0Vyhf+cODD1uVg=="
-        let expectedPrivateKey = "5f6d5afecc677d66fb3d41eee7a8ad8195659ceff588edaf416a9a17daf38fdd"
-        let privateKeyBytes = try! expectedPrivateKey.bytes
-        let privateKey = try! secp256k1.Recovery.PrivateKey(dataRepresentation: privateKeyBytes)
-        let messageData = "We're all Satoshi Nakamoto and a bit of Harold Thomas Finney II.".data(using: .utf8)!
-
-        let recoverySignature = try! privateKey.signature(for: messageData)
-
-        // Verify the recovery signature matches the expected output
-        XCTAssertEqual(expectedRecoverySignature, recoverySignature.dataRepresentation.base64EncodedString())
-
-        let signature = try! recoverySignature.normalize
-
-        // Verify the signature matches the expected output
-        XCTAssertEqual(expectedSignature, signature.dataRepresentation.base64EncodedString())
-        XCTAssertEqual(expectedDerSignature, try! signature.derRepresentation.base64EncodedString())
-    }
-
-    func testPublicKeyRecovery() {
-        let expectedRecoverySignature = "rPnhleCU8vQOthm5h4gX/5UbmxH6w3zw1ykAmLvvtXT4YGKBoiMaP8eBBF8upN8IaTYmO7+o0Vyhf+cODD1uVgE="
-        let expectedPrivateKey = "5f6d5afecc677d66fb3d41eee7a8ad8195659ceff588edaf416a9a17daf38fdd"
-        let privateKeyBytes = try! expectedPrivateKey.bytes
-        let privateKey = try! secp256k1.Recovery.PrivateKey(dataRepresentation: privateKeyBytes)
-        let messageData = "We're all Satoshi Nakamoto and a bit of Harold Thomas Finney II.".data(using: .utf8)!
-
-        let recoverySignature = try! privateKey.signature(for: messageData)
-
-        // Verify the recovery signature matches the expected output
-        XCTAssertEqual(expectedRecoverySignature, recoverySignature.dataRepresentation.base64EncodedString())
-
-        let publicKey = try! secp256k1.Recovery.PublicKey(messageData, signature: recoverySignature)
-
-        // Verify the recovered public key matches the expected public key
-        XCTAssertEqual(publicKey.dataRepresentation, privateKey.publicKey.dataRepresentation)
-    }
-
-    func testSchnorrSigning() {
-        let expectedDerSignature = "6QeDH4CEjRBppTcbQCQQNkvfHF+DB7AITFXxzi3KghUl9mpKheqLceSCp084LSzl6+7o/bIXL0d99JANMQU2wA=="
-        let expectedSignature = "e907831f80848d1069a5371b402410364bdf1c5f8307b0084c55f1ce2dca821525f66a4a85ea8b71e482a74f382d2ce5ebeee8fdb2172f477df4900d310536c0"
-        let expectedPrivateKey = "0000000000000000000000000000000000000000000000000000000000000003"
-        let privateKeyBytes = try! expectedPrivateKey.bytes
-        let privateKey = try! secp256k1.Schnorr.PrivateKey(dataRepresentation: privateKeyBytes)
-        var messageDigest = try! "0000000000000000000000000000000000000000000000000000000000000000".bytes
-
-        var auxRand = try! "0000000000000000000000000000000000000000000000000000000000000000".bytes
-
-        let signature = try! privateKey.signature(message: &messageDigest, auxiliaryRand: &auxRand)
-
-        // Verify the signature matches the expected output
-        XCTAssertEqual(expectedSignature, String(bytes: Array(signature.dataRepresentation)))
-        XCTAssertEqual(expectedDerSignature, signature.dataRepresentation.base64EncodedString())
-    }
 
     func testVerifying() {
         let expectedPrivateKey = "5f6d5afecc677d66fb3d41eee7a8ad8195659ceff588edaf416a9a17daf38fdd"
@@ -458,23 +219,6 @@ final class P256KTests: XCTestCase, @unchecked Sendable {
         XCTAssertEqual(sharedSecret1.bytes, sharedSecret2.bytes)
     }
 
-//    func testKeyAgreementHashFunction() {
-//        let context = secp256k1.Context.rawRepresentation
-//        let privateKey1 = try! secp256k1.KeyAgreement.PrivateKey()
-//        let privateKey2 = try! secp256k1.KeyAgreement.PrivateKey()
-//
-//        var pub = secp256k1_pubkey()
-//        let sharedSecret1 = try! privateKey1.sharedSecretFromKeyAgreement(with: privateKey2.publicKey)
-//        var sharedSecret2 = [UInt8](repeating: 0, count: 32)
-//
-//        XCTAssertEqual(secp256k1_ec_pubkey_parse(context, &pub, privateKey1.publicKey.bytes, privateKey1.publicKey.bytes.count), 1)
-//        XCTAssertEqual(secp256k1_ecdh(context, &sharedSecret2, &pub, privateKey2.baseKey.key.bytes, nil, nil), 1)
-//
-//        let symmerticKey = SHA256.hash(data: sharedSecret1.bytes)
-//
-//        XCTAssertEqual(symmerticKey.bytes, sharedSecret2)
-//    }
-
     func testKeyAgreementPublicKeyTweakAdd() {
         let privateSign1 = try! secp256k1.Signing.PrivateKey()
         let privateSign2 = try! secp256k1.Signing.PrivateKey()
@@ -632,39 +376,6 @@ final class P256KTests: XCTestCase, @unchecked Sendable {
 
         XCTAssertEqual(combinedPublicKey.dataRepresentation.bytes, expectedCombinedKey)
     }
-
-//    func testPubkeyCombineBindings() {
-//        // Initialize context
-//        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_NONE))!
-//
-//        // Destroy context after execution
-//        defer { secp256k1_context_destroy(context) }
-//
-//        // Setup private and public key variables
-//        var pubKeyLen = 33
-//        var cPubKey1 = secp256k1_pubkey()
-//        var cPubKey2 = secp256k1_pubkey()
-//
-//        let publicKeyBytes1 = try! "021b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014".bytes
-//        let publicKeyBytes2 = try! "0260303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752".bytes
-//
-//        // Verify the context and keys are setup correctly
-//        XCTAssertEqual(secp256k1_ec_pubkey_parse(context, &cPubKey1, publicKeyBytes1, pubKeyLen), 1)
-//        XCTAssertEqual(secp256k1_ec_pubkey_parse(context, &cPubKey2, publicKeyBytes2, pubKeyLen), 1)
-//
-//        let pubKeys: [UnsafePointer<secp256k1_pubkey>?] = [UnsafePointer(&cPubKey1), UnsafePointer(&cPubKey2)]
-//        var combinedKey = secp256k1_pubkey()
-//        var combinedKeyBytes = [UInt8](repeating: 0, count: pubKeyLen)
-//
-//        // Combine the two public keys
-//        XCTAssertEqual(secp256k1_ec_pubkey_combine(context, &combinedKey, pubKeys, 2), 1)
-//        XCTAssertEqual(secp256k1_ec_pubkey_serialize(context, &combinedKeyBytes, &pubKeyLen, &combinedKey, secp256k1.Format.compressed.rawValue), 1)
-//
-//        // Define the expected combined key
-//        let expectedCombinedKey = try! "03d6a3a9d62c7650fcac18f9ee68c7a004ebad71b7581b683062213ad9f37ddb28".bytes
-//
-//        XCTAssertEqual(combinedKeyBytes, expectedCombinedKey)
-//    }
 
     func testPrivateKeyPEM() {
         let privateKeyString = """
@@ -824,19 +535,6 @@ final class P256KTests: XCTestCase, @unchecked Sendable {
     }
 
     static let allTests = [
-//        ("testUncompressedKeypairCreation", testUncompressedKeypairCreation),
-//        ("testCompressedKeypairCreation", testCompressedKeypairCreation),
-//        ("testECDHBindings", testECDHBindings),
-//        ("testExtraKeysBindings", testExtraKeysBindings),
-//        ("testRecoveryBindings", testRecoveryBindings),
-//        ("testSchnorrBindings", testSchnorrBindings),
-        ("testCompressedKeypairImplementationWithRaw", testCompressedKeypairImplementationWithRaw),
-        ("testSha256", testSha256),
-        ("testShaHashDigest", testShaHashDigest),
-        ("testRecoverySigning", testRecoverySigning),
-        ("testPublicKeyRecovery", testPublicKeyRecovery),
-        ("testSigning", testSigning),
-        ("testSchnorrSigning", testSchnorrSigning),
         ("testVerifying", testVerifying),
         ("testSchnorrVerifyingPre", testSchnorrVerifyingPre),
         ("testSchnorrVerifying", testSchnorrVerifying),
@@ -853,7 +551,6 @@ final class P256KTests: XCTestCase, @unchecked Sendable {
         ("testZeroization", testZeroization),
         ("testPrivateKeyTweakAdd", testPrivateKeyTweakAdd),
         ("testKeyAgreement", testKeyAgreement),
-//        ("testKeyAgreementHashFunction", testKeyAgreementHashFunction),
         ("testKeyAgreementPublicKeyTweakAdd", testKeyAgreementPublicKeyTweakAdd),
         ("testXonlyToPublicKey", testXonlyToPublicKey),
         ("testTapscript", testTapscript),
@@ -861,7 +558,6 @@ final class P256KTests: XCTestCase, @unchecked Sendable {
         ("testSchnorrNegating", testSchnorrNegating),
         ("testTaprootDerivation", testTaprootDerivation),
         ("testPubkeyCombine", testPubkeyCombine),
-//        ("testPubkeyCombineBindings", testPubkeyCombineBindings),
         ("testPrivateKeyPEM", testPrivateKeyPEM),
         ("testPublicKeyPEM", testPublicKeyPEM),
         ("testSigningPEM", testSigningPEM),
