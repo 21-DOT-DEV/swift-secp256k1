6dfd5bf6429f0275237eb91fb06af94907b4bed9
csjones <csjones@users.noreply.github.com>
2025-04-22 11:36:56 -0700

    Workaround for new XCFramework

--- Tests/XCFrameworkTests/XCFrameworkTests.swift
+++ Tests/XCFrameworkTests/XCFrameworkTests.swift
@@ -17,7 +17,7 @@ struct XCFrameworkTestSuite {
         let expectedPrivateKey = "7da12cc39bb4189ac72d34fc2225df5cf36aaacdcac7e5a43963299bc8d888ed"
         let expectedPublicKey = "023521df7b94248ffdf0d37f738a4792cc3932b6b1b89ef71cddde8251383b26e7"
         let privateKeyBytes = try! expectedPrivateKey.bytes
-        let privateKey = try! secp256k1.Signing.PrivateKey(dataRepresentation: privateKeyBytes)
+        let privateKey = try! P256K.Signing.PrivateKey(dataRepresentation: privateKeyBytes)
 
         #expect(String(bytes: privateKey.dataRepresentation) == expectedPrivateKey)
         #expect(String(bytes: privateKey.publicKey.dataRepresentation) == expectedPublicKey)
