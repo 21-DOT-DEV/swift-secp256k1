6f7b51095956d0e0866d16b5953e3e9208a18e05
csjones <csjones@users.noreply.github.com>
2025-10-25 19:24:14 -0700

    Extracted needed source files from libsecp256k1

--- Sources/libsecp256k1/src/modules/musig/main_impl.h
+++ Sources/libsecp256k1/src/modules/musig/main_impl.h
@@ -1,1 +1,12 @@
-../../../../../Submodules/secp256k1/src/modules/musig/main_impl.h
+/**********************************************************************
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#ifndef SECP256K1_MODULE_MUSIG_MAIN_H
+#define SECP256K1_MODULE_MUSIG_MAIN_H
+
+#include "keyagg_impl.h"
+#include "session_impl.h"
+
+#endif
