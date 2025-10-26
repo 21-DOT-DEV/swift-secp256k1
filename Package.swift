e65aebf561c0f4d9178a3530cb2a37c0f74214be
csjones <csjones@users.noreply.github.com>
2025-10-25 18:13:26 -0700

    Remove secp256k1 submodule

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         .package(url: "https://github.com/csjones/lefthook-plugin.git", exact: "2.0.0"),
         .package(url: "https://github.com/21-DOT-DEV/swift-plugin-tuist.git", exact: "4.83.0"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat.git", exact: "0.58.5"),
-        .package(url: "https://github.com/realm/SwiftLint.git", exact: "0.62.1")
+        .package(url: "https://github.com/realm/SwiftLint.git", exact: "0.62.1"),
+        .package(url: "https://github.com/21-DOT-DEV/swift-plugin-subtree.git", exact: "0.0.2")
     ],
     targets: [
         .target(name: "P256K", dependencies: ["libsecp256k1"]),
