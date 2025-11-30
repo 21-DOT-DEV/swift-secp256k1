07940a76e299829e84a7acc7e94a0bf67d7130a7
csjones <csjones@users.noreply.github.com>
2025-11-29 20:29:05 -0800

    Remove swift-crypto submodule

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/21-DOT-DEV/swift-plugin-tuist.git", exact: "4.83.0"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat.git", exact: "0.58.5"),
         .package(url: "https://github.com/realm/SwiftLint.git", exact: "0.62.1"),
-        .package(url: "https://github.com/21-DOT-DEV/swift-plugin-subtree.git", exact: "0.0.3")
+        .package(url: "https://github.com/21-DOT-DEV/swift-plugin-subtree.git", exact: "0.0.4")
     ],
     targets: [
         .target(name: "P256K", dependencies: ["libsecp256k1"]),
