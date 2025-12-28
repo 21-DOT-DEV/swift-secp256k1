6c193456cd082167e77484c7973ef153ead2052a
csjones <csjones@users.noreply.github.com>
2025-12-28 02:41:31 -0800

    Squashed 'Vendor/swift-crypto/' content from commit 45305d32
    git-subtree-dir: Vendor/swift-crypto
    git-subtree-split: 45305d32cfb830faebcaa9a7aea66ad342637518

---
+++ Sources/Crypto/ASN1/Basic ASN1 Types/ASN1OctetString.swift
@@ -0,0 +1,60 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftCrypto open source project
+//
+// Copyright (c) 2019-2020 Apple Inc. and the SwiftCrypto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftCrypto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+#if CRYPTO_IN_SWIFTPM && !CRYPTO_IN_SWIFTPM_FORCE_BUILD_API
+@_exported import CryptoKit
+#else
+import Foundation
+
+extension ASN1 {
+    /// An octet string is a representation of a string of octets.
+    struct ASN1OctetString: ASN1ImplicitlyTaggable {
+        static var defaultIdentifier: ASN1.ASN1Identifier {
+            .primitiveOctetString
+        }
+
+        var bytes: ArraySlice<UInt8>
+
+        init(asn1Encoded node: ASN1.ASN1Node, withIdentifier identifier: ASN1.ASN1Identifier) throws {
+            guard node.identifier == identifier else {
+                throw CryptoKitASN1Error.unexpectedFieldType
+            }
+
+            guard case .primitive(let content) = node.content else {
+                preconditionFailure("ASN.1 parser generated primitive node with constructed content")
+            }
+
+            self.bytes = content
+        }
+
+        init(contentBytes: ArraySlice<UInt8>) {
+            self.bytes = contentBytes
+        }
+
+        func serialize(into coder: inout ASN1.Serializer, withIdentifier identifier: ASN1.ASN1Identifier) throws {
+            coder.appendPrimitiveNode(identifier: identifier) { bytes in
+                bytes.append(contentsOf: self.bytes)
+            }
+        }
+    }
+}
+
+extension ASN1.ASN1OctetString: Hashable { }
+
+extension ASN1.ASN1OctetString: ContiguousBytes {
+    func withUnsafeBytes<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
+        return try self.bytes.withUnsafeBytes(body)
+    }
+}
+
+#endif // Linux or !SwiftPM
